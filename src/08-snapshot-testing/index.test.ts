b8b1a5b2a2374433b7bd9f0c3bba77fc3503a3ed
vladlutskevich <vlad01309@mail.ru>
2024-10-21 16:43:19 +0300

    feat: implement snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,31 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+/* eslint-disable prettier/prettier */
+import { generateLinkedList } from './index';
+
+const linkedList = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    },
+  },
+};
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3, 4])).toStrictEqual(linkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3, 4])).toMatchSnapshot();
   });
 });
