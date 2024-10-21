ac229255643b7a9b191fad4d3dfe99493df5b22c
vladlutskevich <vlad01309@mail.ru>
2024-10-21 16:30:00 +0300

    feat: implement mocking-lib-api

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,32 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+/* eslint-disable prettier/prettier */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+const baseURL = 'https://jsonplaceholder.typicode.com';
+const relativePath = '/albums/5';
+
+jest.mock('lodash', () => ({
+  throttle: (fn: () => unknown) => fn,
+}));
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(relativePath);
+    expect(axios.create).toHaveBeenCalledWith({ baseURL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const getMock = jest.fn().mockResolvedValue({ data: {} });
+    axios.create = jest.fn().mockReturnValue({ get: getMock });
+    await throttledGetDataFromApi(relativePath);
+    expect(getMock).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const getMock = jest.fn().mockResolvedValue({ data: { id: 5 } });
+    axios.create = jest.fn().mockReturnValue({ get: getMock });
+    const result = await throttledGetDataFromApi(relativePath);
+    expect(result).toEqual({ id: 5 });
   });
 });
